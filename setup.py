096543ecb3becfe9ffe836b4f08f8426fa56cad7
Jason R. Coombs <jaraco@jaraco.com>
2018-08-17 09:36:42 -0400

    Move scripts to runpy-runnable modules

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Exposing the Google Voice "API" to the Python language
 -------------------------------------------------------
 
 Google Voice for Python Allows you to place calls, send sms, download voicemail, and check the various folders of your Google Voice Accounts.
-You can use the Python API or command line script to schedule calls, check for new recieved calls/sms, or even sync your recorded voicemails/calls.  
+You can use the Python API or command line script to schedule calls, check for new recieved calls/sms, or even sync your recorded voicemails/calls.
 Works for Python 2 and Python 3
 
 Full documentation is available up at http://sphinxdoc.github.com/pygooglevoice/
@@ -25,7 +25,6 @@ params = dict(
     description = 'Python 2/3 Interface for Google Voice',
     long_description = README,
     packages = ['googlevoice'],
-    scripts = ['bin/gvoice','bin/asterisk-gvoice-setup', 'bin/gvi'],
     install_requires=['six', 'requests'],
 )
 
