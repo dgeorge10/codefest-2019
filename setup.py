9d4f34705be762fe791fb728a980ad40a35b25e9
Jason R. Coombs <jaraco@jaraco.com>
2018-08-17 08:53:12 -0400

    Delint and update URL

--- setup.py
+++ setup.py
@@ -10,22 +10,22 @@ Exposing the Google Voice "API" to the Python language
 -------------------------------------------------------
 
 Google Voice for Python Allows you to place calls, send sms, download voicemail, and check the various folders of your Google Voice Accounts.
-You can use the Python API or command line script to schedule calls, check for new recieved calls/sms, or even sync your recorded voicemails/calls.  
+You can use the Python API or command line script to schedule calls, check for new recieved calls/sms, or even sync your recorded voicemails/calls.
 Works for Python 2 and Python 3
 
 Full documentation is available up at http://sphinxdoc.github.com/pygooglevoice/
 """
 
 params = dict(
-    name = "googlevoice",
-    version = '1.0',
-    url = 'https://bitbucket.org/jaraco/googlevoice',
-    author = 'Jason R. Coombs',
+    name="googlevoice",
+    version='1.0',
+    url='https://github.com/jaraco/pygooglevoice',
+    author='Jason R. Coombs',
     author_email='jaraco@jaraco.com',
-    description = 'Python Interface for Google Voice',
-    long_description = README,
-    packages = ['googlevoice'],
-    scripts = ['bin/gvoice','bin/asterisk-gvoice-setup', 'bin/gvi'],
+    description='Python Interface for Google Voice',
+    long_description=README,
+    packages=['googlevoice'],
+    scripts=['bin/gvoice', 'bin/asterisk-gvoice-setup', 'bin/gvi'],
     install_requires=['six', 'requests'],
 )
 
