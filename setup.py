55bfa3b75243164dd851a301159ac564ef5f5a97
Jason R. Coombs <jaraco@jaraco.com>
2018-08-17 08:54:45 -0400

    Have just one README text

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 import setuptools
 
 
-README = """Python Google Voice
-====================
-
-Based on "pygooglevoice" by Joe McCall & Justin Quick
-
-Exposing the Google Voice "API" to the Python language
--------------------------------------------------------
-
-Google Voice for Python Allows you to place calls, send sms, download voicemail, and check the various folders of your Google Voice Accounts.
-You can use the Python API or command line script to schedule calls, check for new recieved calls/sms, or even sync your recorded voicemails/calls.
-Works for Python 2 and Python 3
-
-Full documentation is available up at http://sphinxdoc.github.com/pygooglevoice/
-"""
-
 params = dict(
     name="googlevoice",
     version='1.0',
@@ -23,7 +8,6 @@ params = dict(
     author='Jason R. Coombs',
     author_email='jaraco@jaraco.com',
     description='Python Interface for Google Voice',
-    long_description=README,
     packages=['googlevoice'],
     scripts=['bin/gvoice', 'bin/asterisk-gvoice-setup', 'bin/gvi'],
     install_requires=['six', 'requests'],
