05a34d0a94824e0f474f0d9e7fdc613ad5b3c192
steve <stevinnh@gmail.com>
2019-05-04 14:35:53 -0400

    some get functions for guest

--- routes/guests.js
+++ routes/guests.js
@@ -1,11 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
+const Op = Sequelize.Op;
 
 const guest = require("../models/Guest")
 
 router.get("/", (req,res) => {
     guest.findAll()
+    .then(guest => res.send(guest))
+    .catch(err => console.log(err))
+});
+
+router.get("/:number", (req,res) => {
+    guest.findAll({
+        where: {
+            number: req.params["number"]
+        }
+    })
+    .then(guest => res.send(guest))
+    .catch(err => console.log(err))
+});
+
+router.get("/men", (req,res) => {
+    guest.findAll({
+        where: {
+            gender: "male"
+        }
+    })
+    .then(guest => res.send(guest))
+    .catch(err => console.log(err))
+});
+
+router.get("/women", (req,res) => {
+    guest.findAll({
+        where: {
+            gender: "female"
+        }
+    })
+    .then(guest => res.send(guest))
+    .catch(err => console.log(err))
+});
+
+router.get("/families", (req,res) => {
+    guest.findAll({
+        where: {
+            dependents: {
+                [Op.gt]: 0
+            }
+        }
+    })
     .then(guest => res.send(guest))
     .catch(err => console.log(err))
 });
