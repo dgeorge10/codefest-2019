c4cb60540e81e65fd1e76c1307a17f6fa51b1fff
Dennis George <djg365@drexel.edu>
2019-05-04 14:17:25 -0400

    connecting guest model

--- models/Guest.js
+++ models/Guest.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../config/database');
 
 //model for a user
-const Guest = db.define('guest', {
+const Guest = db.define('Guest', {
     number:{
         type: Sequelize.STRING
     },
