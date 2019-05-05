b532ba1bfbde08513add945db153d1c5da51bfaf
Dennis George <djg365@drexel.edu>
2019-05-05 11:11:15 -0400

    adding path reference

--- routes/food.js
+++ routes/food.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 const sequelize = require("sequelize")
-
+const path = require("path");
 const food = require("../models/Food")
 
 router.get("/getCurrentDay", (req,res) => {
